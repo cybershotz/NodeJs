5de186cbc43734a6be6a97e9e4ae03d8ab3151b6
Ammar <ammar4568@gmail.com>
2020-01-31 17:18:24 +0500

    9. Understanding Spread & Rest Operators

--- 02 JavaScript Refresher/09UnderstandingSpread&RestOperators.js
+++ 02 JavaScript Refresher/09UnderstandingSpread&RestOperators.js
@@ -1,6 +1,16 @@
 const hobbies = ['Sports', 'Cooking'];
 const copiedHobbies = [...hobbies, 'Basketball']; // Spread Operator
 
+const person = {
+    Name: 'John',
+    Age: 20,
+    greet() {
+        console.log('Hi ' + this.Name + ' Age => ' + this.Age)
+    } 
+} 
+
+const copiedPerson = {...person} 
+
 const toArray = (...args) => { // Rest Operator
     return args;
 }
