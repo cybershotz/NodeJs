6f8f879f59dbc93e1c238d593bc4494f56cd157a
Ammar <ammar4568@gmail.com>
2020-02-03 23:11:44 +0500

    9. Parsing Incoming Requests

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Hello from Add Product!</h1>')
+    res.send(
+        '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>'
+    )
+})
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    next();
 })
 
 app.use('/', (req, res, next) => {
