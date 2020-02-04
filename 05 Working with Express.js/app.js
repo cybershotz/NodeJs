326df52a5ea8255c8c7cf4c6d3951bc2cbefe0e8
Ammar <ammar4568@gmail.com>
2020-02-04 12:57:29 +0500

    19. Serving Files Statically

--- 05 Working with Express.js/app.js
+++ 05 Working with Express.js/app.js
@@ -9,7 +9,8 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
-  
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/admin', adminRoutes); 
 app.use(shopRoutes); 
 
