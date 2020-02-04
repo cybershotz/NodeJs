978cac986a73ad6538838bbf7cac7e8d3dc0659d
Ammar <ammar4568@gmail.com>
2020-02-04 22:54:37 +0500

    11. Converting our Project to Handlebars

--- 06 Working with Dynamic Content & Adding Templating Engines/app.js
+++ 06 Working with Dynamic Content & Adding Templating Engines/app.js
@@ -3,12 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const expressHbs = require('express-handlebars')
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug')
+
+app.engine('hbs', expressHbs({
+    defaultLayout: ''
+}))
+app.set('view engine', 'hbs')
+// app.set('view engine', 'pug')
 app.set('views', 'views') // Load Views from 'views' folder
 
 app.use(bodyParser.urlencoded({ extended: false }))
