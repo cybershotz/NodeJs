b7d357a617441bdee65ed7c4d233592dab62af4a
Ammar <ammar4568@gmail.com>
2020-02-04 21:02:15 +0500

    4. Installing & Implementing Pug

--- 06 Working with Dynamic Content & Adding Templating Engines/app.js
+++ 06 Working with Dynamic Content & Adding Templating Engines/app.js
@@ -8,6 +8,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', 'views') // Load Views from 'views' folder
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
