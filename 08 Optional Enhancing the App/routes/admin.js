97af9ab723871b0b974d7fb8533520dc57fbff00
Ammar <ammar4568@gmail.com>
2020-02-05 12:30:16 +0500

    4. Registering the Routes

--- 08 Optional Enhancing the App/routes/admin.js
+++ 08 Optional Enhancing the App/routes/admin.js
@@ -2,13 +2,18 @@ const path = require('path');
 
 const express = require('express')
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// /admin/add-product
+router.get('/add-product', adminController.getAddProduct)
 
-router.get('/add-product', productsController.getAddProduct)
 
-router.post('/add-product', productsController.postAddProduct)
+// /admin/products
+router.get('/products', adminController.getProducts)
+
+// /admin/add-product
+router.post('/add-product', adminController.postAddProduct)
 
 module.exports = router;
