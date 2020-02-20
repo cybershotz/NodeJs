69eec0b243b9c7621e4272c56f2b5b0b180aaf24
Ammar <ammar4568@gmail.com>
2020-02-20 23:29:22 +0500

    12. Deleting Products

--- 11 Understanding Sequelize/controllers/admin.js
+++ 11 Understanding Sequelize/controllers/admin.js
@@ -70,8 +70,14 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+    .then(product => {
+        return product.destroy();
+    })
+    .then(result => {
+        res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err))
 }
 
 exports.getProducts = (req, res, next) => {
