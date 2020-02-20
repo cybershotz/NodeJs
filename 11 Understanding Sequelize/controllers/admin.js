52ad183ad7437ec4bfbadcaaef7e27a064ff27d1
Ammar <ammar4568@gmail.com>
2020-02-20 23:25:40 +0500

    11. Updating Products

--- 11 Understanding Sequelize/controllers/admin.js
+++ 11 Understanding Sequelize/controllers/admin.js
@@ -33,17 +33,19 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/')
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
-            return res.redirect('/')
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/')
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
         })
-    })
+        .catch(err => console.log(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
@@ -52,9 +54,18 @@ exports.postEditProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
     const price = req.body.price;
-    const updatedProduct = new Product(prodId, title, imageUrl, description, price);
-    updatedProduct.save()
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+    .then(product => {
+        product.title = title;
+        product.imageUrl = imageUrl;
+        product.description = description;
+        product.price = price;
+        return product.save();
+    })
+    .then(result => {
+        res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err)) 
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -65,14 +76,14 @@ exports.postDeleteProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.findAll()
-    .then(products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            })
         })
-    })
-    .catch(err => {
-        console.log('err');
-    })
+        .catch(err => {
+            console.log('err');
+        })
 }
