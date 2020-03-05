9e7c1d9de082d4007240c899810f44986ff78fe7
Ammar <ammar4568@gmail.com>
2020-03-05 22:33:00 +0500

    7. Finishing the Database Connection

--- 12 Working with NoSQL & Using MongoDB/app.js
+++ 12 Working with NoSQL & Using MongoDB/app.js
@@ -3,16 +3,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
-const mongoConnect = require('./util/database')
+const mongoConnect = require('./util/database').mongoConnect;
 
 const errorController = require('./controllers/error')
 
-
-
 const app = express();
-
 
 app.set('view engine', 'ejs')
 app.set('views', 'views') // Load Views from 'views' folder
@@ -29,12 +26,11 @@ app.use((req, res, next) => { // On All Incoming Request this gets executed
     //     .catch(err => console.log(err))
 })
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404)
 
-mongoConnect(client => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(3000);
 })
