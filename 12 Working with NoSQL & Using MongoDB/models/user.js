3a124abd2c76b8bc406cdb3d07cd730e355f3267
Ammar <ammar4568@gmail.com>
2020-03-06 23:10:58 +0500

    21. Working on Cart Items & Orders

--- 12 Working with NoSQL & Using MongoDB/models/user.js
+++ 12 Working with NoSQL & Using MongoDB/models/user.js
@@ -1,15 +1,27 @@
 const mongoDb = require('mongodb')
 const getDb = require('../util/database').getDb;
-
+const ObjectID = mongoDb.ObjectID;
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        const updatedCart = { items: [{ ...product, quantity: 1 }] }
+        const db = getDb();
+        db.collection('users')
+            .updateOne(
+                { _id: new ObjectID(this._id) },
+                { $set: { cart: updatedCart } }
+            )
     }
 
     static findById(userId) {
