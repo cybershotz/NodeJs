2767ea319d0fbb77b45b83a70d62c7206f8ee8e7
Ammar <ammar4568@gmail.com>
2020-03-06 15:42:24 +0500

    11. Fetching All Products

--- 12 Working with NoSQL & Using MongoDB/app.js
+++ 12 Working with NoSQL & Using MongoDB/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 const mongoConnect = require('./util/database').mongoConnect;
 
 const errorController = require('./controllers/error')
@@ -28,7 +28,7 @@ app.use((req, res, next) => { // On All Incoming Request this gets executed
 })
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404)
 
