abba0a5185595f0de0508ece4f4c89d8dda6cf2c
Ammar <ammar4568@gmail.com>
2020-03-06 19:21:45 +0500

    19. Creating New Users

--- 12 Working with NoSQL & Using MongoDB/app.js
+++ 12 Working with NoSQL & Using MongoDB/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user')
 
 const errorController = require('./controllers/error')
 
@@ -18,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => { // On All Incoming Request this gets executed
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user; // Add a Sequelize Use object to request for upcoming middlewares 
-    //         next();
-    //     })
-    //     .catch(err => console.log(err))
+    User.findById("5e625bbd6a81f71b906680b1")
+        .then(user => {
+            req.user = user; 
+            next();
+        })
+        .catch(err => console.log(err))
     next();
 })
 
--- 12 Working with NoSQL & Using MongoDB/models/user.js
+++ 12 Working with NoSQL & Using MongoDB/models/user.js
@@ -1,16 +1,21 @@
-const Sequelize = require('sequelize');
+const mongoDb = require('mongodb')
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
+    }
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false, 
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({ id: new mongoDb.ObjectID(userId) })
+    }
+}
 
 module.exports = User;
