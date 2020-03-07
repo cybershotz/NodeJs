593c15ab39eb1b1d067fba8bcf74ff2617630190
Ammar <ammar4568@gmail.com>
2020-03-07 12:50:58 +0500

    1. Module Introduction

---
+++ 13 Working with Mongoose/controllers/admin.js
@@ -0,0 +1,83 @@
+const Product = require('../models/product')
+
+exports.getAddProduct = (req, res, next) => {
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    })
+}
+
+exports.postAddProduct = (req, res, next) => {
+    // console.log(req.body);
+    title = req.body.title;
+    imageUrl = req.body.imageUrl;
+    description = req.body.description;
+    price = req.body.price;
+    const product = new Product(title, price, description, imageUrl, null, req.user._id);
+    product.save()
+        .then(() => {
+            res.redirect('/admin/products')
+        }).catch(err => {
+            console.log(err)
+        })
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/')
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/')
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
+        })
+        .catch(err => console.log(err))
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const description = req.body.description;
+    const price = req.body.price;
+    const product = new Product(title, price, description, imageUrl, prodId)
+    product.save()
+        .then(result => {
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId)
+        .then(result => {
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
+}
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            })
+        })
+        .catch(err => {
+            console.log('err');
+        })
+}
