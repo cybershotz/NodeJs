a12807fcb35205e977e54673e1dfca0efa6714c8
Ammar <ammar4568@gmail.com>
2020-03-07 10:23:40 +0500

    22. Adding the Add to Cart Functionality

--- 12 Working with NoSQL & Using MongoDB/app.js
+++ 12 Working with NoSQL & Using MongoDB/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => { // On All Incoming Request this gets executed
     User.findById("5e625bbd6a81f71b906680b1")
         .then(user => {
-            req.user = user; 
+            req.user = new User(user.name, user.email, user.cart, user._id) ; 
             next();
         })
         .catch(err => console.log(err))
--- 12 Working with NoSQL & Using MongoDB/models/user.js
+++ 12 Working with NoSQL & Using MongoDB/models/user.js
@@ -15,7 +15,7 @@ class User {
     }
 
     addToCart(product) {
-        const updatedCart = { items: [{ ...product, quantity: 1 }] }
+        const updatedCart = { items: [{ productId: new ObjectID(product._id), quantity: 1 }] }
         const db = getDb();
         db.collection('users')
             .updateOne(
