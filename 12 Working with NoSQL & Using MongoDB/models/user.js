07be03ce196e38387ad53c423bf404ccda36dade
Ammar <ammar4568@gmail.com>
2020-03-07 10:58:04 +0500

    23. Storing Multiple Products in the Cart

--- 12 Working with NoSQL & Using MongoDB/models/user.js
+++ 12 Working with NoSQL & Using MongoDB/models/user.js
@@ -15,7 +15,20 @@ class User {
     }
 
     addToCart(product) {
-        const updatedCart = { items: [{ productId: new ObjectID(product._id), quantity: 1 }] }
+        console.log('prodID', this._id)
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            console.log('cp', cp)
+            return cp.productId.toString() === product._id.toString()
+        })
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({ productId: new ObjectID(product._id), quantity: 1 })
+        }
+        const updatedCart = { items: updatedCartItems }
         const db = getDb();
         db.collection('users')
             .updateOne(
