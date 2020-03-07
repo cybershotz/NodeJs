8c9da59b550351a551179d14598a626a100a18f5
Ammar <ammar4568@gmail.com>
2020-03-07 11:24:42 +0500

    27. Adding an Order

--- 12 Working with NoSQL & Using MongoDB/controllers/shop.js
+++ 12 Working with NoSQL & Using MongoDB/controllers/shop.js
@@ -103,28 +103,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
     let fetchedCart;
     req.user
-        .getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts()
-        })
-        .then(products => {
-            req.user.createOrder()
-                .then(order => {
-                    return order.addProducts(
-                        products.map(product => {
-                            console.log('product', product.cartItem.quantity);
-                            product.orderItem = { quantity: product.cartItem.quantity };
-                            return product;
-                        })
-                    );
-                })
-                .catch(err => console.log(err))
-            // console.log(products);
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null);
-        })
+        .addOrder()
         .then(result => {
             res.redirect('/orders')
         })
