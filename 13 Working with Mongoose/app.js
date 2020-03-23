64cea8814e6376c93c34bbdf7f08ec370300a145
Ammar <ammar4568@gmail.com>
2020-03-23 19:44:51 +0500

    10. Adding and Using a User Model

--- 13 Working with Mongoose/app.js
+++ 13 Working with Mongoose/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const User = require('./models/user')
+const User = require('./models/user')
 
 const errorController = require('./controllers/error')
 
@@ -18,14 +18,14 @@ app.set('views', 'views') // Load Views from 'views' folder
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => { // On All Incoming Request this gets executed
-//     User.findById("5e625bbd6a81f71b906680b1")
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err))
-// })
+app.use((req, res, next) => { // On All Incoming Request this gets executed
+    User.findById("5e78cb0081e4412910e9bde5")
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,18 @@ app.use(errorController.get404)
 
 mongoose.connect('mongodb+srv://ammar:HSDI2cHcKTqdBx4s@cluster0-mylyc.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
+        User.findOne(user => {
+            if (!user) {
+                const newUser = new User({
+                    name: 'Ammar',
+                    email: 'ammar@test.com',
+                    cart: {
+                        items: []
+                    }
+                })
+                newUser.save();
+            }
+        })
         app.listen(3000);
     })
     .catch(err => console.log(err))
