7375233a1f51521e0310bdeb2637cdf9580d93bb
Ammar <ammar4568@gmail.com>
2020-03-24 21:38:51 +0500

    4. Optional Creating the Login Form

--- 14 Sessions & Cookies/app.js
+++ 14 Sessions & Cookies/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const User = require('./models/user')
 
 const errorController = require('./controllers/error')
@@ -29,6 +30,7 @@ app.use((req, res, next) => { // On All Incoming Request this gets executed
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404)
 
