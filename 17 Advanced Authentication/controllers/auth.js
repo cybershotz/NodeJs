4eb711b708294f73030f92b9f101c9451240a469
Ammar <ammar4568@gmail.com>
2020-03-28 13:07:45 +0500

    3. Implementing the Token Logic

--- 17 Advanced Authentication/controllers/auth.js
+++ 17 Advanced Authentication/controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
@@ -118,5 +119,37 @@ exports.getReset = (req, res, next) => {
         path: '/reset',
         pageTitle: 'Reset Password',
         errorMessage: message
+    })
+}
+
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log('err')
+            return res.redirect('/reset')
+        }
+        const token = buffer.toString();
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 360000; // One hour from now
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/')
+                return transporter.sendMail({
+                    to: req.body.email,
+                    from: 'shop@node-complete.com',
+                    subject: 'Password Reset',
+                    html: `
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="https://localhost:3000/reset/${token}">link</a> to set a new password</p>
+
+                    `
+                })
+            })
+            .catch(err => console.log(err))
+
     })
 }
