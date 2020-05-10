14aac31e6c685b160eeec8cbe54cb5ec36a1aaea
Ammar <ammar4568@gmail.com>
2020-05-10 19:29:02 +0500

    20. File Upload & Download

---
+++ 20 File Upload and Download/app.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf')
+const flash = require('connect-flash')
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+const User = require('./models/user')
+
+const errorController = require('./controllers/error')
+
+const MONGODB_URI = 'mongodb+srv://ammar:HSDI2cHcKTqdBx4s@cluster0-mylyc.mongodb.net/shop'
+
+const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
+const csrfProtection = csrf()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views') // Load Views from 'views' folder
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}))
+app.use(csrfProtection);
+app.use(flash())
+
+app.use((req, res, next) => { // On All Incoming Request this gets executed
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch(err => { throw new Error(err) })
+})
+
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken()
+    res.locals.isAuthenticated = req.session.isLoggedIn
+    next()
+
+})
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500)
+
+app.use(errorController.get404)
+
+app.use((error, req, res, next) => {
+    res.redirect('/500')
+})
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => console.log(err))
