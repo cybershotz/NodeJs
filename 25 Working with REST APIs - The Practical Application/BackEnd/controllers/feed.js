12c56cae48ca3f5b568a06adca4ee3cfeedd06e7
Ammar <ammar4568@gmail.com>
2020-05-12 15:12:32 +0500

    7. Adding Server Side Validation

--- 25 Working with REST APIs - The Practical Application/BackEnd/controllers/feed.js
+++ 25 Working with REST APIs - The Practical Application/BackEnd/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -18,6 +20,15 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422) // 422 => Validation Failed
+            .json({
+                message: 'Validation failed, entered data is incorrect',
+                errors: errors.array()
+            })
+    }
 
     res.status(201).json({ // Status 201 means that a resource was also created on our side and success
         message: 'Post created successfully',
