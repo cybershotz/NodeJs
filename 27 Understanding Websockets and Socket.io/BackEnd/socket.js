984ff27c15cdd22aae3bf3d573f0eb771984e5ee
Ammar <ammar4568@gmail.com>
2020-05-13 22:14:48 +0500

    7. Sharing the IO Instance Across Files

--- 27 Understanding Websockets and Socket.io/BackEnd/socket.js
+++ 27 Understanding Websockets and Socket.io/BackEnd/socket.js
@@ -4,5 +4,10 @@ module.exports = {
     init: httpServer => {
         io = require('socket.io')(httpServer);
         return io;
+    },
+    getIO: () => {
+        if (!io)
+            throw new Error('Socket.io is not initialized')
+        return io;
     }
 }
